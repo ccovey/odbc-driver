2b1a69952a7bde38baa6eec2755bd4503a94d517
Cody Covey <ccovey14@gmail.com>
2013-01-02 22:16:31 -0800

    Fix Formatting

--- src/ODBCDriver/ODBCConnector.php
+++ src/ODBCDriver/ODBCConnector.php
@@ -1,46 +1,44 @@
-<?php 
+<?php
+
 namespace Ccovey\ODBCDriver;
 
 use Illuminate\Database\Connectors as Connectors;
 
 class ODBCConnector extends Connectors\Connector implements Connectors\ConnectorInterface {
 
-	/**
-	 * Establish a database connection.
-	 *
-	 * @param  array  $options
-	 * @return PDO
-	 */
-	public function connect(array $config)
-	{
-		$dsn = $this->getDsn($config);
+    /**
+     * Establish a database connection.
+     *
+     * @param  array  $options
+     * @return PDO
+     */
+    public function connect(array $config) {
+        $dsn = $this->getDsn($config);
 
-		// We need to grab the PDO options that should be used while making the brand
-		// new connection instance. The PDO options control various aspects of the
-		// connection's behavior, and some might be specified by the developers.
-		$options = $this->getOptions($config);
+        // We need to grab the PDO options that should be used while making the brand
+        // new connection instance. The PDO options control various aspects of the
+        // connection's behavior, and some might be specified by the developers.
+        $options = $this->getOptions($config);
 
-		$connection = $this->createConnection($dsn, $config, $options);
+        $connection = $this->createConnection($dsn, $config, $options);
 
-		return $connection;
-	}
+        return $connection;
+    }
 
-	/**
-	 * Create a DSN string from a configuration.
-	 *
-	 * @param  array   $config
-	 * @return string
-	 */
-	protected function getDsn(array $config)
-	{
-		// First we will create the basic DSN setup as well as the port if it is in
-		// in the configuration options. This will give us the basic DSN we will
-		// need to establish the PDO connections and return them back for use.
-		extract($config);
+    /**
+     * Create a DSN string from a configuration.
+     *
+     * @param  array   $config
+     * @return string
+     */
+    protected function getDsn(array $config) {
+        // First we will create the basic DSN setup as well as the port if it is in
+        // in the configuration options. This will give us the basic DSN we will
+        // need to establish the PDO connections and return them back for use.
+        extract($config);
 
-		$dsn = "odbc:{$dsn}; {$username}; {$password};";
-         
-		return $dsn;
-	}
+        $dsn = "odbc:{$dsn}; {$username}; {$password};";
 
+        return $dsn;
+    }
 }
